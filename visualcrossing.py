7eac79e98dd7ba1e1632e1f67af57d8f54dd85b3
U.C.S.Z <support@tp-new.com>
2023-02-16 11:17:47 +0800

    Update visualcrossing.py

--- visualcrossing.py
+++ visualcrossing.py
@@ -14,27 +14,12 @@ import json
 import SendMail
 import ProcessingDataUnit
 
-try:
-    key_value = os.environ["VISUALCROSSING_KEY"]  # visualcrossing key
-    ResultBytes = urllib.request.urlopen(
-        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/HangZhou%20City?unitGroup=metric&include=events%2Cdays%2Ccurrent%2Calerts&key=" + key_value + "&contentType=json")
-    #  Parse the results as JSON
-    jsonData = json.load(ResultBytes)
-    maillog = ProcessingDataUnit.weatherJsonToString(jsonData)
-    # print(maillog)
-    to_addr = os.environ["MASTER_MAIL_SMTP_TOADDRESS"]  # 接收邮箱
-    mail_arg = os.environ["MASTER_MAIL_SMTP_163QY_SSL"]  # 发送协议地址
-    # mail_port = os.environ["MASTER_MAIL_SMTP_SSL_PORT"]  # 发送协议端口
-    mail_account = os.environ["MASTER_MAIL_SMTP_FROMADDRESS"]  # 发送邮件账号
-    mail_password = os.environ["MASTER_MAIL_SMTP_163QY_PASSWORD"]  # 发送邮件账号密码
-    strSub = '出行前置信息之天气汇总' + time.strftime("%Y%m%d%H%M%S%p")  # 邮件标题
-    SendMail.sendMail(mail_account, mail_password, mail_arg, to_addr, strSub, str(maillog))
-    ProcessingDataUnit.file_in("weather", maillog)
-except urllib.error.HTTPError as e:
-    ErrorInfo = e.read().decode()
-    print('Error code: ', e.code, ErrorInfo)
-    sys.exit()
-except urllib.error.URLError as e:
-    ErrorInfo = e.read().decode()
-    print('Error code: ', e.code, ErrorInfo)
-    sys.exit()
+maillog = ProcessingDataUnit.splicingString()
+to_addr = os.environ["MASTER_MAIL_SMTP_TOADDRESS"]  # 接收邮箱
+mail_arg = os.environ["MASTER_MAIL_SMTP_163QY_SSL"]  # 发送协议地址
+# mail_port = os.environ["MASTER_MAIL_SMTP_SSL_PORT"]  # 发送协议端口
+mail_account = os.environ["MASTER_MAIL_SMTP_FROMADDRESS"]  # 发送邮件账号
+mail_password = os.environ["MASTER_MAIL_SMTP_163QY_PASSWORD"]  # 发送邮件账号密码
+strSub = '出行前置信息之天气汇总' + time.strftime("%Y%m%d%H%M%S%p")  # 邮件标题
+SendMail.sendMail(mail_account, mail_password, mail_arg, to_addr, strSub, str(maillog))
+ProcessingDataUnit.file_in("weather", maillog)
