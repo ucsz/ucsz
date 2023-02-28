4aba94cbd85bdd0646eeb534798a7644786966b1
U.C.S.Z <support@tp-new.com>
2023-02-28 10:24:21 +0800

    Update WeatherProcessing.py

--- WeatherProcessing.py
+++ WeatherProcessing.py
@@ -18,6 +18,27 @@ import requests
 # 处理数据格式
 
 
+# 设置请求头
+def getHeadders():
+    header = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Host': 'devapi.qweather.com',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Sec-GPC': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent=': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0 Chrome/100.0 Safari/800.0'
+    }
+    return header
+
+
 # 将所有方法获取的字串进行拼接
 def splicingString():
     strLogAllDate = "\n=================================" + qweatherWeatherJsonToString
