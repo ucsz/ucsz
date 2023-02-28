21564420def1948ab05278069f06453db50e9ea2
U.C.S.Z <support@tp-new.com>
2023-02-28 18:02:40 +0800

    Update WeatherProcessing.py

--- WeatherProcessing.py
+++ WeatherProcessing.py
@@ -56,8 +56,7 @@ def qweatherWeatherJsonToString():
     strLogAllDayDate = ""
     key_value = os.environ["QWEATHERAPI_KEY"]  # visualcrossing key
     try:
-        resultBytes = urllib.request.urlopen(
-            "https://devapi.qweather.com/v7/weather/7d?location=101201301&key=" + key_value)
+        resultBytes = urllib.request.urlopen("https://devapi.qweather.com/v7/weather/7d?location=101201301&key=" + key_value)
         resultJson = '{}'  # 将返回对象转化为json串
         # 通过header中的Content-Encoding标记判断是否会Gzip压缩
         if resultBytes.info().get('Content-Encoding') == 'gzip':
